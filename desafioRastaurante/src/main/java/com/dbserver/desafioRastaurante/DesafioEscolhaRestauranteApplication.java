765aaf95fd056e907538143d6fed180fda03768f
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-09-16 11:45:15 -0300

    Criado o projeto na IDE STS

---
+++ desafioRastaurante/src/main/java/com/dbserver/desafioRastaurante/DesafioEscolhaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.dbserver.desafioRastaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEscolhaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEscolhaRestauranteApplication.class, args);
+	}
+
+}
