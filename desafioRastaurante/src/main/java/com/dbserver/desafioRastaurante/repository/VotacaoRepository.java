a15044b6a128eca89fe0b634fb16ba571edd6457
Guilherme Gomes Palma <guilherme.palma@dbserver.com.br>
2019-09-17 14:45:01 -0300

    Ajusta path repositoy

---
+++ desafioRastaurante/src/main/java/com/dbserver/desafioRastaurante/repository/VotacaoRepository.java
@@ -0,0 +1,17 @@
+package com.dbserver.desafioRastaurante.repository;
+
+import java.util.Calendar;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dbserver.desafioRastaurante.entity.Votacao;
+
+@Repository
+public interface VotacaoRepository extends JpaRepository<Votacao, Integer> {
+	
+	
+	@Transactional(readOnly = true)
+	Votacao findByDataVotacao(Calendar dataVotacao);
+}
--- desafioRastaurante/src/main/java/com/dbserver/desafioRastaurante/repository/VotoRepository.java
+++ desafioRastaurante/src/main/java/com/dbserver/desafioRastaurante/repository/VotoRepository.java
@@ -1,4 +1,4 @@
-package repository;
+package com.dbserver.desafioRastaurante.repository;
 
 import java.util.Date;
 
